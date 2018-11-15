b55b786155638029755546dbf4b078635c14f855
Nikola Kasev <nikola.kasev@gmail.com>
2018-11-15 07:06:31 -0800

    mvn quickstart

---
+++ src/main/java/com/ing/baker/tutorials/App.java
@@ -0,0 +1,13 @@
+package com.ing.baker.tutorials;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
