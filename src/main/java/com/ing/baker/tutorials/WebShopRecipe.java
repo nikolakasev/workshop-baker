9fcfc0ae61d6e1ba1750aabe25acc5b386fc2c2b
Nikola Kasev <nikola.kasev@gmail.com>
2018-11-22 12:27:55 +0100

    ready for prime time at BBConf 2018

--- src/main/java/com/ing/baker/tutorials/WebShopRecipe.java
+++ src/main/java/com/ing/baker/tutorials/WebShopRecipe.java
@@ -1,11 +1,14 @@
 package com.ing.baker.tutorials;
 
 import com.ing.baker.recipe.javadsl.Recipe;
+import com.ing.baker.tutorials.interactions.ValidateOrder;
 
 import static com.ing.baker.recipe.javadsl.InteractionDescriptor.of;
 
 class WebShopRecipe {
     static Recipe getRecipe() {
-        return null;
+        //TODO implement the recipe according to the sequence diagram in docs/sequence.png
+        return new Recipe("WebShop")
+                .withInteraction(of(ValidateOrder.class));
     }
 }
